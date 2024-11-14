e2d333e89280510e7177a7face13ecb7346f99ae
harry <harrisongitau3@gmail.com>
2024-11-14 17:41:25 +0300

    Update to flask

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_moment import Moment
 
 
 app = Flask(__name__)
@@ -11,5 +13,8 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+login = LoginManager(app)
+login.login_view = 'login'
+moment = Moment(app)
 
 from app import routes, models
---
+++ app/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from app.models import User
+
+
+
+class LoginForm(FlaskForm):
+    """Login Form"""
+    username = StringField('Username', validators=[DataRequired(), Length(1, 16)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    """Registration Form"""
+    username = StringField('Username', validators=[DataRequired(), Length(1, 16)])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Login')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
